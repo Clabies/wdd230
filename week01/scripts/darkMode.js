bb39ec2535b897c74b2a3246e763fcc1c825722c
Clabies <abiesparzasa@gmail.com>
2024-03-22 07:58:04 -0500

    Update

---
+++ week01/scripts/darkMode.js
@@ -0,0 +1,24 @@
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+let originalLinkColor = null;
+
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("🕶️")) {
+        main.style.background = "rgb(0, 0, 0)";
+        main.style.color = "rgb(255, 255, 255)";
+        main.querySelectorAll("a").forEach(link => {
+            if (!originalLinkColor) {
+                originalLinkColor = getComputedStyle(link).color;
+            }
+            link.style.color = "rgb(255, 255, 255)";
+        });
+        modeButton.textContent = "👓";
+    } else {
+        main.style.background = "rgb(255, 255, 255)";
+        main.style.color = "rgb(0, 0, 0)";
+        main.querySelectorAll("a").forEach(link => {
+            link.style.color = originalLinkColor;
+        });
+        modeButton.textContent = "🕶️";
+    }
+});
